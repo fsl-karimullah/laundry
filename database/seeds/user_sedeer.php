ed1910e7bed61bcc854a4291b0a8acf3b7b0c884
fsl-karimullah <--replace-all>
2023-06-25 16:30:25 -0700

    first commit

--- database/seeds/user_sedeer.php
+++ database/seeds/user_sedeer.php
@@ -9,23 +9,23 @@ class user_sedeer extends Seeder
      *
      * @return void
      */
-    // public function run()
-    // {
-    //     $name = ['Admin','Super Admin','User'];
+    public function run()
+    {
+        $name = ['Admin','Super Admin','User'];
 
-    //     for($i=0;$i<count($name);$i++){
-    //         $role = App\Role::create([
-    //             'name'=> $name[$i],
-    //             'slug'=> str_slug($name[$i])
-    //         ]);
+        for($i=0;$i<count($name);$i++){
+            $role = App\Role::create([
+                'name'=> $name[$i],
+                'slug'=> str_slug($name[$i])
+            ]);
 
-    //         $user = App\User::create([
-    //             'name'=> $name[$i],
-    //             'username'=> str_slug($name[$i]),
-    //             'role_id'=> $role->id,
-    //             'email'=> str_slug($name[$i])."@google.com",
-    //             'password'=> Hash::make('rahasia'),
-    //         ]);
-    //     }
-    // }
+            $user = App\User::create([
+                'name'=> $name[$i],
+                'username'=> str_slug($name[$i]),
+                'role_id'=> $role->id,
+                'email'=> str_slug($name[$i])."@google.com",
+                'password'=> Hash::make('rahasia'),
+            ]);
+        }
+    }
 }
